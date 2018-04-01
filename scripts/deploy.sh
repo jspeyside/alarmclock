af55d30ed2a048cf019e4dc71d7370be5cc11b86
James Grady <jlgrady1@gmail.com>
2018-03-31 23:19:09 -0700

    Fixed upload script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,9 @@ set -e
 b2 authorize-account $B2_ID $B2_KEY
 VERSION=${VERSION:-`cat VERSION`}
 
-if (! docker images | grep -q speyside/alarmclock | grep -q $VERSION); then
+if (! docker images | grep speyside/alarmclock | grep -q $VERSION); then
   scripts/build.sh
 fi
-
-go get -t -u -v ./...
-mkdir -p build
-rm -rf build/*
 
 # Build the binaries
 cd build/
@@ -18,8 +14,9 @@ build_os=(darwin linux windows)
 for os in ${build_os[@]}; do
   cp alarmclock_${os} alarmclock
   tar -czvf alarmclock_${VERSION}_${os}.tar.gz alarmclock
-  b2 upload-file boomerain-web alarmclock_${VERSION}.tar.gz binaries/alarmclock/${VERSION}/alarmclock_${VERSION}_${os}.tar.gz
+  b2 upload-file boomerain-web alarmclock_${VERSION}_${os}.tar.gz binaries/alarmclock/${VERSION}/alarmclock_${VERSION}_${os}.tar.gz
 done
+cd ..
 rm -rf build/*
 
 docker push speyside/alarmclock:$VERSION speyside/alarmclock:latest
